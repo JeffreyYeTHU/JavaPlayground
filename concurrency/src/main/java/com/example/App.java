f3dc332aee83ba6d190bd9a867a86bdd02d9e272
Jeffrey <yejian_1988@126.com>
2021-07-04 21:21:33 +0800

    extract method

--- concurrency/src/main/java/com/example/App.java
+++ concurrency/src/main/java/com/example/App.java
@@ -1,20 +1,19 @@
 package com.example;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class App {
-    private static int count = 0;
-    private static Lock lock = new ReentrantLock();
 
     public static void main(String[] args) {
+        demoThreadNotSafe();
+    }
+
+    private static void demoThreadNotSafe() {
         ThreadPoolExecutor threadPool = (ThreadPoolExecutor) Executors.newCachedThreadPool();
         for (int i = 0; i < 1000; i++) {
-            threadPool.submit(new Add());
+            threadPool.submit(new Incrementor());
         }
 
         try {
@@ -22,17 +21,7 @@ public class App {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println(count);
+        System.out.println(Incrementor.getCount());
         System.out.println("largest thread pool size:" + threadPool.getLargestPoolSize());
-    }
-
-    static class Add implements Runnable {
-
-        @Override
-        public void run() {
-            // lock.lock();
-            count++;
-            // lock.unlock();
-        }
     }
 }
