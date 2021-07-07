5b898956487ae780532ec38ee6f1a0c753b4089e
Jeffrey <780336375@qq.com>
2021-07-07 12:08:05 +0800

    Init a spring gradle project

---
+++ spring-gradle-demo/src/test/java/com/example/springgradledemo/SpringGradleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgradledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGradleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
