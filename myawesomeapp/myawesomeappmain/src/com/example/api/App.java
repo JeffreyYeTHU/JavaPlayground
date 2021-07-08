7511fe59612a9fc7e420833009bfe55af012713d
Jeffrey <yejian_1988@126.com>
2021-07-08 22:55:12 +0800

    Add muti project example

---
+++ myawesomeapp/myawesomeappmain/src/com/example/api/App.java
@@ -0,0 +1,10 @@
+package com.example.api;
+
+import com.example.model.*;
+
+public class App {
+    public static void main(String[] args){
+        var p = new Person("Jeffrey", "Ye");
+        System.out.println(p);
+    }
+}
